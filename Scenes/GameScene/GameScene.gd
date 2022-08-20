3dbe8b3b6a4f82e602c44eeec2bb0e99bac92914
Maaack <marek.belski@gmail.com>
2022-08-19 23:45:49 -0700

    Stop audio of breathing when player dies or succeeds.

--- Scenes/GameScene/GameScene.gd
+++ Scenes/GameScene/GameScene.gd
@@ -22,12 +22,17 @@ func _on_MoonWorld_player_right_foot_grounded(state):
 func _on_MoonWorld_player_y_rotated(value):
 	$MeteorsViewport/Viewport/MoonMiniature.rotate_camera_y(value)
 
+func stop_audio():
+	$AudioPlayers/Breathing.stop()
+
 func _on_MoonWorld_human_died(reason):
+	stop_audio()
 	InGameMenuController.open_menu(death_screen_packed, false)
 	if InGameMenuController.current_menu.has_method("set_reason"):
 		InGameMenuController.current_menu.set_reason(reason)
 
 func _on_MoonWorld_succeeded(rest_stops):
+	stop_audio()
 	InGameMenuController.open_menu(success_screen_packed, false)
 	if InGameMenuController.current_menu.has_method("set_rest_stops"):
 		InGameMenuController.current_menu.set_rest_stops(rest_stops)
