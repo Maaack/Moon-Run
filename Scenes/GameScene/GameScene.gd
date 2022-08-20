af248d4e88a9725ce3d647aa2005609e50fba9f0
Maaack <marek.belski@gmail.com>
2022-08-19 22:46:13 -0700

    Connects human dead signals.

--- Scenes/GameScene/GameScene.gd
+++ Scenes/GameScene/GameScene.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var death_screen_packed = preload("res://Scenes/DeathScreen/DeathScreen.tscn")
+
 func _on_MoonWorld_player_camera_x_rotated(value):
 	$HelmetViewport/Viewport/Helmet.rotate_camera_x(value)
 	$MeteorsViewport/Viewport/MoonMiniature.rotate_camera_x(value)
@@ -18,3 +20,8 @@ func _on_MoonWorld_player_right_foot_grounded(state):
 
 func _on_MoonWorld_player_y_rotated(value):
 	$MeteorsViewport/Viewport/MoonMiniature.rotate_camera_y(value)
+
+func _on_MoonWorld_human_died(reason):
+	InGameMenuController.open_menu(death_screen_packed, false)
+	if InGameMenuController.current_menu.has_method("set_reason"):
+		InGameMenuController.current_menu.set_reason(reason)
