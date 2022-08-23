63843cc28f0dd0d9cb727b52e198980833426e1f
Maaack <marek.belski@gmail.com>
2022-08-22 21:37:52 -0700

    Makes the lights in the helmet for feet contact more visible and useful.

--- Scenes/UI/OxygenMeter.gd
+++ Scenes/UI/OxygenMeter.gd
@@ -1,16 +1,25 @@
 extends Spatial
 
-const green_color = Color("#08ff00")
-const red_color = Color("#ff1200")
+export(Color) var text_color : Color
+export(Color) var alert_color : Color
 
-var oxygen = 100 setget set_oxygen
+var oxygen : float = 1.0 setget set_oxygen
+var warning_level : float = 0.2
+var alert : bool = false setget set_alert
 
+func set_alert(value : bool) -> void:
+	if value == alert:
+		return
+	alert = value
+	if alert:
+		$Label3D.modulate = alert_color
+	else:
+		$Label3D.modulate = text_color
 
-func set_oxygen(value : int):
+func set_oxygen(value : float) -> void:
 	oxygen = value
-	var nb_lights_to_activate = floor((oxygen+19)/20) as int # the +19 is here so the nb_lights is 1-5 and 0 means shut all,
-#else at 0 the first one stays on
-	for i in range(5):
-		$Lights.get_child(i).toggle(i < nb_lights_to_activate)
-	$Label3D.modulate = green_color if oxygen > 0 else red_color
-
+	var light_count = $Lights.get_child_count()
+	var lights_to_activate = ceil(light_count*oxygen)
+	for i in light_count:
+		$Lights.get_child(i).toggle(i < lights_to_activate)
+	set_alert(oxygen < warning_level)
