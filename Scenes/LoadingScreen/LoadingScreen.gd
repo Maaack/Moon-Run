2d0b66724c80af3ff8d74313a8d756f913a59580
Maaack <marek.belski@gmail.com>
2022-08-27 18:05:30 -0700

    Fixes loading screen by adding a child instance instead of calling change scene.

--- Scenes/LoadingScreen/LoadingScreen.gd
+++ Scenes/LoadingScreen/LoadingScreen.gd
@@ -1,25 +1,29 @@
-extends Control
+extends CanvasLayer
 
+const LOADING_COMPLETE_TEXT = "Loading Complete!"
 
 onready var loader = ResourceLoader.load_interactive(SceneLoader.scene_to_load)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func set_new_scene(scene_resource : Resource):
+	var scene_instance : Node = scene_resource.instance()
+	scene_instance.connect("ready", self, "queue_free")
+	get_node("/root").add_child(scene_instance)
+	get_tree().current_scene = scene_instance
+
 func _process(_delta):
 	var err = loader.poll()
 	if err == OK:
-		$VBoxContainer/ProgressBar.value = loader.get_stage() * 100 / loader.get_stage_count()
+		$Control/VBoxContainer/ProgressBar.value = loader.get_stage() * 100 / loader.get_stage_count()
 	elif err == ERR_FILE_EOF:
-		$VBoxContainer/ProgressBar.value = 100
+		$Control/VBoxContainer/ProgressBar.value = 100
+		$Control/VBoxContainer/Title.text = LOADING_COMPLETE_TEXT
 		set_process(false)
 		yield(get_tree().create_timer(0.1), "timeout")
-		err = get_tree().change_scene_to(loader.get_resource())
-		if err:
-			$ErrorMsg.dialog_text = "Scene Change Error: %d" % err
-			$ErrorMsg.popup_centered()
+		set_new_scene(loader.get_resource())
 	else:
-		$ErrorMsg.dialog_text = "Loading Error: %d" % err
-		$ErrorMsg.popup_centered()
-
+		$Control/ErrorMsg.dialog_text = "Loading Error: %d" % err
+		$Control/ErrorMsg.popup_centered()
+		set_process(false)
 
 func _on_ErrorMsg_confirmed():
 	var err = get_tree().change_scene(ProjectSettings.get_setting("application/run/main_scene"))
